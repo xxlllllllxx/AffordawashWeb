a51a83e21b8a718a37a731a6ea7e8a99d3041e77
Lewis Masallo <lewismasallo@gmail.com>
2022-12-01 13:20:06 +0800

    initial commit

---
+++ application/controllers/Employee.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Employee extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('login');
+    }
+}
