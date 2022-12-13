e4fd1f6b13a12a84b5014a245930a10221352dbb
Lewis Masallo <lewismasallo@gmail.com>
2022-12-13 18:39:35 +0800

    db-cleared

--- db_affordawash/db_affordawash.sql
+++ db_affordawash/db_affordawash.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2022 at 09:08 AM
+-- Generation Time: Dec 13, 2022 at 11:28 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -42,14 +42,14 @@ CREATE TABLE `tbl_customer_transact` (
 --
 
 INSERT INTO `tbl_customer_transact` (`id`, `customer_alias`, `employee_id`, `machine_id_list`, `item_id_list`, `transaction_payment`, `transaction_datetime`) VALUES
-(1, 'Diero', 1, '1 200', '1 3 30:2 5 60', 159, '12/07/2022 11:48:38 am'),
 (2, 'Diero', 5, '1 200', '1 3 30:4 5 60', 159, '12/07/2022 11:48:39 am'),
 (3, 'Diero', 1, '1 200', '1 3 30:2 5 60', 159, '12/07/2022 11:48:40 am'),
 (4, 'Diero', 1, '1 200', '1 3 30:2 5 60', 159, '12/07/2022 11:48:41 am'),
 (5, 'Diero', 1, '1 200', '1 3 30:2 5 60', 159, '12/07/2022 11:55:35 am'),
 (6, 'Diero', 1, '1 200', '1 3 30:2 5 60', 159, '12/07/2022 11:56:55 am'),
-(7, 'Diero', 1, '6 200', '1 3 30:2 5 60', 159, '12/07/2022 12:34:06 pm'),
-(8, 'Diero', 1, '1 200', '1 3 30:2 5 60', 159, '12/07/2022 01:53:24 pm');
+(7, 'Diero', 1, '6 200', '5 2 16:8 2 22', 159, '12/07/2022 12:34:06 pm'),
+(12, 'fgdfg', 29, '', '', 0, 'June'),
+(13, 'fff', 29, '6 54', '5 3 24:7 2 0', 0, 'June');
 
 -- --------------------------------------------------------
 
@@ -70,18 +70,17 @@ CREATE TABLE `tbl_employee` (
 --
 
 INSERT INTO `tbl_employee` (`id`, `employee_username`, `employee_password`, `name`, `employee_salary`) VALUES
-(1, 'lewis', '1234', 'Lewis Masallo', 222),
-(5, 'trial', '1234', 'My trial employee', 550),
-(27, 'lewismasallo', '1234', 'Lewis', 55034),
-(28, 'gf', '1234', 'dfs', 55),
 (29, '23', '123', '123', 123),
-(30, '213', '213', '213', 23),
 (31, 'fsd', 'sdf', 'fdsdf', 550),
 (32, '1234', '1234', 'calim', 124),
 (33, '123', '213', 'dfs', 123),
 (34, 'fds', 'fdsdf', 'fddsf', 550),
 (35, 'dfs', 'dfsd', 'fdsf', 550),
-(36, 'll', 'll', 'll', 550);
+(36, 'll', 'll', 'll', 550),
+(37, 'gff', 'g', 'Lewis', 550),
+(38, 'reter', 'rtert', 'rtet', 550),
+(40, 'sgsg', 'gsg', 'sgsg', 550),
+(41, 'fg', 'fg5', 'fg', 45);
 
 -- --------------------------------------------------------
 
@@ -103,13 +102,12 @@ CREATE TABLE `tbl_item` (
 --
 
 INSERT INTO `tbl_item` (`id`, `item_name`, `item_quantity`, `item_cost`, `item_lowest_price`, `item_selling_price`) VALUES
-(1, 'Surf', 38, 81, 95, 43),
-(2, 'Surfa', 504, 6, 8, 0),
-(3, 'Surfing', 50, 6, 8, 0),
-(4, 'Surfings', 50, 6, 8, 8),
 (5, 'Surfd', 50, 6, 8, 8),
-(6, 'Surf12', 12, 12, 12, 0),
-(7, 'Surf333', 333, 33, 33, 0);
+(7, 'Surf333', 333, 33, 33, 0),
+(8, 'item 2', 11, 11, 11, 11),
+(9, '11', 11, 11, 11, 11),
+(10, '', 0, 0, 0, 0),
+(11, 'hdh', 4, 543, 45, 45);
 
 -- --------------------------------------------------------
 
@@ -131,11 +129,7 @@ CREATE TABLE `tbl_machine` (
 --
 
 INSERT INTO `tbl_machine` (`id`, `service_name`, `washing`, `drying`, `washing_price`, `drying_price`) VALUES
-(1, 'MyService', 'true', 'false', 20, 20),
-(2, 'Super', 'true', 'true', 33, 22),
-(3, 'Supert', 'false', 'true', 0, 545),
 (4, '214', 'true', 'true', 44, 545),
-(5, 'rreer', 'true', 'false', 3424, 0),
 (6, 'gggg', 'false', 'true', 0, 54),
 (7, 'fggfgf', 'true', 'false', 56, 0),
 (8, 'er', 'true', 'false', 0, 0);
@@ -207,19 +201,19 @@ ALTER TABLE `tbl_manager`
 -- AUTO_INCREMENT for table `tbl_customer_transact`
 --
 ALTER TABLE `tbl_customer_transact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_employee`
 --
 ALTER TABLE `tbl_employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `tbl_item`
 --
 ALTER TABLE `tbl_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tbl_machine`
