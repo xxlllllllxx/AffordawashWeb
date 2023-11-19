097590fa25c52166c060f5e4ff9d35de91bb1a70
Lewis Daveriel Masallo <lewismasallo@outlook.com>
2023-11-19 14:44:00 +0800

    path_info to query string

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/AffordawashWeb/';
+$config['base_url'] = 'http://localhost/Affordawash_web/';
 
 /*
 |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']    = 'PATH_INFO';
+$config['uri_protocol']    = 'QUERY_STRING';
 
 
 /*
